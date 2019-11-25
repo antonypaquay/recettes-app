f9481e2dbdb93fec57805161be1a87617e466e45
Antony PAQUAY <apaquay@adneom.com>
2019-11-25 23:26:44 +0100

    ci(): add all date for the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,29 @@
 import React from "react";
 
-const Card = ({details}) => {
-    return (
-        <div className="card">
-            <div className="recette">
-                <h2>{details.nom}</h2>
-            </div>
-        </div>
-    )
-}
+const Card = ({ details }) => {
+  // formatage liste des ingrédients
+  const ingredients = details.ingredients
+    // split permet de transformer une chaine en tableau grâce à un séparateur
+    .split(",")
+    .map(item => <li key={item}>{item}</li>);
+
+  // formatage liste des instructions
+  const instructions = details.instructions
+    .split("\n")
+    .map(item => <li key={item}>{item}</li>);
+
+  return (
+    <div className="card">
+      <div className="image">
+        <img src={require(`../img/${details.image}`)} alt={details.nom} />
+      </div>
+      <div className="recette">
+        <h2>{details.nom}</h2>
+        <ul className="liste-ingredients">{ingredients}</ul>
+        <ol className="liste-instruction">{instructions}</ol>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
